473084697070e71266a14b9f375233524de7193b
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-09-23 19:35:31 +0900

    Slice API result

--- app/controllers/api/watched_repositories_controller.rb
+++ app/controllers/api/watched_repositories_controller.rb
@@ -1,7 +1,13 @@
 class Api::WatchedRepositoriesController < ApplicationController
   def index
     client = Octokit::Client.new(access_token: session[:access_token])
-    repos = client.watched
-    render json: repos.map(&:to_h)
+    repos = client.watched.map(&:to_h).map do |repo|
+      {
+        owner: repo[:owner].slice(:id, :login),
+        **repo.slice(:id, :name, :full_name, :private)
+      }
+    end
+
+    render json: repos
   end
 end
