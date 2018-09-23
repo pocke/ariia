9697952c4f604b21a8ea4766ecbdbaee98e0ed36
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-09-23 18:29:45 +0900

    Do not need assets and helpers

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module Ariia
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators.assets false
+    config.generators.helper false
   end
 end
